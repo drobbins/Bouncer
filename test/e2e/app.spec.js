4d485396160e3642e3f712744bfdfedb46938455
David Robbins <robbins.david@gmail.com>
2014-03-17 10:00:45 -0500

    An initial protractor test

---
+++ test/e2e/app.spec.js
@@ -0,0 +1,8 @@
+describe("hello-protractor", function () {
+    describe("index", function () {
+        it("Should display the correct title", function () {
+            browser.get("/#");
+            expect(browser.getTitle()).toBe("Hello");
+        });
+    });
+});
